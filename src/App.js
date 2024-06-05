792c15514a243b36a7b0642fa18bbea0782cca7d
uzairsaeed34 <uzairsaeed34k@gmail.com>
2024-06-05 07:04:53 +0500

    updated code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Login from './Login';
 import Registration from './Registration';
 import { ToastContainer } from 'react-toastify';
 
+
+// json-server --watch db.json --port 8000 ---- for local storage 
+// npm start
+
+
 function App() {
   return (
     <div className="App">
